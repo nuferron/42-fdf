9e3a092c7b6b844aa8e4e133258d8f1e6b291f24
Nuria Ferron Ferriz <nuriaf207@gmail.com>
2023-08-15 16:05:46 +0200

    printing an image of 4x4

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
 #include "minilibx/mlx.h"
 #include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 
 typedef struct	s_win
 {
@@ -11,22 +13,37 @@ typedef struct	s_win
 
 typedef struct	s_img
 {
-	t_win	win;
+	t_win	*win;
 	void	*img_ptr;
 	char	*addr;
 	int		height;
 	int		width;
 	int		bpp;
 	int		endian;
-	int		line_len
+	int		line_len;
 }				t_img;
 
 
-int	main()
+t_win	*new_program(int width, int height, char *str)
 {
 	t_win	*win;
 
 	win = (t_win *)malloc(sizeof(t_win));
+	if (!win)
+		return (win);
+	win->mlx_init = mlx_init();
+	win->mlx_wdw = mlx_new_window(win->mlx_init, width, height, str);
+	if (!win->mlx_wdw)
+		return (win);
+	return (win);
+}
+
+int	main()
+{
+	t_win	*win;
+	t_img	*img4;
+
+	/*win = (t_win *)malloc(sizeof(t_win));
 	if (!win)
 		return (-1);
 	win->mlx_init = mlx_init();
@@ -36,8 +53,19 @@ int	main()
 	win->width = 600;
 	win->mlx_wdw = mlx_new_window(win->mlx_init, win->width, win->height, "FDF");
 	if (!win->mlx_wdw)
+		return (-1);*/
+	win = new_program(300, 300, "FDF");
+	if (!win)
 		return (-1);
-	mlx_loop(win->mlx_wdw);
+	img4 = (t_img *)malloc(sizeof(t_img));
+	img4->win = win;
+	img4->img_ptr = mlx_new_image(win->mlx_init, 4, 4);
+	img4->addr = mlx_get_data_addr(img4->img_ptr, &(img4->bpp), &(img4->line_len), &(img4->endian));
+	img4->width = 4;
+	img4->height = 4;
+	memcpy(img4->addr, "s4vfs4vfs4vfs4vfs4vfs4vfs4vfs4vfs4vfs4vfs4vfs4vfs4vfs4vfs4vfs4vf", 16*4);
+	mlx_put_image_to_window(img4->win->mlx_init, img4->win->mlx_wdw,img4->img_ptr, 10, 10);
+	mlx_loop(win->mlx_init);
 }
 
 /*int	main()
