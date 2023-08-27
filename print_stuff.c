4ec11e1a9aa9e3a454afbc2810e5db97e9dfb3f2
nuferron <nuriaf207@gmail.com>
2023-08-27 20:19:10 +0200

    Added function for colors converison

--- print_stuff.c
+++ print_stuff.c
@@ -1,5 +1,15 @@
 #include "fdf.h"
 
+unsigned int trgb_to_hex(int t, int r, int g, int b) {
+    return ((t & 0xFF) << 24) | ((r & 0xFF) << 16) | ((g & 0xFF) << 8) | (b & 0xFF);
+}
+
+/*void print_pixel(t_data *data, t_point pixel) {
+
+    char *dst = data->addr + ((int)pixel.y * data->line_len + (int)pixel.x * (data->bpp / 8));
+    *(unsigned int *)dst = pixel.color;
+}*/
+
 void	print_pixel(t_data *data, int x, int y, int color)
 {
 	char	*dst;
@@ -9,7 +19,7 @@ void	print_pixel(t_data *data, int x, int y, int color)
 	*(unsigned int *)dst = color;
 }
 
-void	print_square(t_data *data, t_square *square)
+/*void	print_square(t_data *data, t_square *square)
 {
 	int	i;
 	int	xo;
@@ -31,4 +41,4 @@ void	print_square(t_data *data, t_square *square)
 		(square->y)++;
 		i++;
 	}
-}
+}*/
