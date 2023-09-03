0e1f929e209c708a2e595d4a86c56c17053b5097
Nuria Ferron Ferriz <nuriaf207@gmail.com>
2023-09-03 23:29:05 +0200

    added file so fdf.h doesn't look so messy

---
+++ macros.h
@@ -0,0 +1,14 @@
+#ifndef MACROS_H
+# define MACROS_H
+
+# define WIN_MAX		1300
+# define ESC			53
+# define LEFT			123
+# define RIGHT			124
+# define DOWN			125
+# define UP				126
+# define Q				12
+# define SCROLL_UP		4
+# define SCROLL_DOWN	5
+
+#endif
