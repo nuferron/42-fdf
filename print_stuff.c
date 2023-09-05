c8dd09854fdced3f6d83197dea60a00120d16c41
Nuria Ferron Ferriz <nuriaf207@gmail.com>
2023-09-05 23:19:57 +0200

    not really changed, just debugging modifications

--- print_stuff.c
+++ print_stuff.c
@@ -9,15 +9,9 @@ void	print_pixel(t_data *data, int x, int y, int color)
 	char	*dst;
 
 	if (x < 0 || x >= MAX_X)
-	{
-		//printf("x error %d\n", x);
 		return ;
-	}
 	if (y < 0 || y >= MAX_Y)
-	{
-		//printf("y error %d\n", y);
 		return ;
-	}
 	dst = data->addr + (y * data->line_len + x * (data->bpp / 8));
 	*(unsigned int *)dst = color;
 }
