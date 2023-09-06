29e03459fe8d59e0c095b387cc75dcb68378317a
Nuria Ferron Ferriz <nuriaf207@gmail.com>
2023-09-06 21:32:49 +0200

    it's supposed to work now. maybe there are some leaks

--- read_map.c
+++ read_map.c
@@ -16,154 +16,95 @@
 //	mapa++
 //return points
 
-int	find_sep(char *str, char c)
-{
-	int	pos;
-
-	printf("hola?\nc %c\tstr %s\n", c, str);
-	if (!str)
-		return (-1);
-	pos = 0;
-	while (str[pos] != c && str[pos] != '\0')
-		pos++;
-	if (str[pos] == '\0')
-		return (-1);
-	printf("pos= %d\n", pos);
-	return (pos);
-}
-
-void	read_map(int fd, t_structs *all)
+char	*getting_row(t_structs *all, int fd)
 {
 	char	*line;
-	char	**points_lst;
-	t_point	**map;
-	char	**color;
-	int		x;
-	int		y;
-	t_point	*point;
-
-	y = 0;
-	while (1)
-	{
-		printf("lectura %d\n", y);
-		line = get_next_line(fd);
-		printf("line = %s\n", line);
-		if (!line)
-			break ;
-		all->max_col = ft_strlen(line);
-		points_lst = ft_split(line, ' ');
-		if (!points_lst)
-		{
-			//free them all
-			ft_printf("Uh-oh split error\n");
-		}
-		x = 0;
-		while (points_lst[x][1] != '\0' && points_lst[x][1] != '\n')
-		{
-			printf("volta %d\n", x);
-			point = (t_point *)malloc(sizeof (t_point));
-			if (!point)
-			{
-				//free them all
-				ft_printf("Uh-oh malloc error\n");
-				exit(-1);
-			}
-			point->x = x;
-			point->y = y;
-			point->z = ft_atoi(points_lst[x]);
-			color = ft_split(points_lst[x], ',');
-			printf("mitja marato\n");
-			if (!color)
-			{
-				//free them all
-				ft_printf("Uh-oh malloc error\n");
-			}
-			if (color[1] != NULL)
-				point->color = getting_color(color[1]);
-			else
-				point->color = 0xFF0000;
-			printf("while %c\n", points_lst[x][0]);
-			map[y][x] = *point; // he de reservar memoria per map, pero encara no se quantes files tindre. Pero no passa res, perque puc reservar linies t_point * i despres a cada volta fer free + malloc o algo aixi
-			x++;
-		}
-		y++;
-	}
-	//si el nombre de files son les que haurien de ser, ok si
-	all->max_row = y;
-	close(fd);
-	int i = 0;
-	int j;
-	while (i < all->max_row)
-	{
-		j = 0;
-		while (j < all->max_col)
-		{
-			printf("x %f\ty %f\tcolor %X\n", map[i][j].x, map[i][j].y, map[i][j].color);
-			j++;
-		}
-		i++;
-	}
-}
-
-/*void	read_map(int fd, t_data *data)
-{
-	char	*line;
-	char	**points;
-	char	**color;
-	//t_point	**map;
-	t_point	*point;
-	int		color_sep;
+	int		len;
 
 	line = get_next_line(fd);
-	line = ft_strdup(line);
-	points = ft_split(line, ' ');
-	if (!points)
+	if (!line)
+		return (NULL);
+	len = ft_wordcount(line, ' ');
+	if (all->max_col != 0 && all->max_col != len)
 	{
-		//free them all
-		ft_printf("Uh-oh malloc error\n");
-		exit(-1);
+		ft_printf("Watch out! This map is irregular!\n");
+		return (NULL);
 	}
-	point = (t_point *)malloc(sizeof (t_point));
-	if (!point)
-	{
-		//free them all
-		ft_printf("Uh-oh malloc error\n");
-		exit(-1);
-	}
+	all->max_col = len;
+	return (line);
+}
 
-	point->x = 0;
-	point->y = 0;
-	point->z = ft_atoi(points[0]);
+t_point	*getting_row_points(t_structs all, char **coords)
+{
+	t_point	*row_points;
+	int		col;
+	char	**color;
 
-	//color = ft_split(points[0], ',');
-//	if (!color)
-//	{
-		//free them all
-//		ft_printf("Uh-oh malloc error\n");
-//		exit(-1);
-//	}
-	color_sep = find_sep(points[0], ',');
-	printf("hi ha coma? %d\ncolor: %s\n", color_sep, points[0]);
-	color = ft_split(points[0], ',');
-	if (color[1] != NULL)
+	col = 0;
+	row_points = (t_point *)malloc(sizeof(t_point) * all.max_col);
+	if (!row_points)
+		return (NULL);
+	while (col < all.max_col)
 	{
-		printf("Color specified\n");
-	}
-	else
-		printf("No color specified\n");
-	if (color_sep != -1)
-	{
-		color = ft_split(points[0], ',');
+		row_points[col].x = col;
+		row_points[col].y = all.max_row;
+		row_points[col].z = ft_atoi(coords[col]);
+		color = ft_split(coords[col], ',');
 		if (!color)
 		{
 			//free them all
-			ft_printf("Uh-oh malloc error\n");
-			exit(-1);
+			free_them_all(all, color, 0, 0);
+			//ft_printf("Uh-oh there seems to be some memory problems\n");
+			return (NULL);
 		}
-		point->color = getting_color(color[1]);
+		if (color[1])
+			row_points[col].color = getting_color(color[1]);
+		else
+			row_points[col].color = 0x00FF0000;
+		col++;
 	}
-	else
-		point->color = 0xFF0000;
-	print_pixel(data, 300, 300, point->color);
-	printf("x %f\t y %f\tz %f\ncolor %X\n", point->x, point->y, point->z, point->color);
-}*/
+	return (row_points);
+}
+
+t_list	*read_map(int fd, t_structs *all)
+{
+	t_list	*map;
+	t_list	*tmp;
+	t_point	*points;
+	char	*row;
+	char	**coords;
+
+	all->max_col = 0;
+	all->max_row = 0;
+	map = NULL;
+	while (1)
+	{
+		row = getting_row(all, fd);
+		if (!row)
+			break ;
+		coords = ft_split(row, ' ');
+		if (!coords)
+		{
+			//free them all
+			//ft_printf("Uh-oh there seems to be some memory problems\n");
+			return ((t_list *)free_them_all(*all, coords, map, points));
+		}
+		points = getting_row_points(*all, coords);
+		if (!points)
+		{
+			//free them all
+			//ft_printf("Uh-oh there seems to be some memory problems\n");
+			return ((t_list *)free_them_all(*all, coords, map, points));
+		}
+		tmp = ft_lstnew(points);
+		if (!tmp)
+		{
+			//free them all
+			//ft_printf("Uh-oh there seems to be some memory problems\n");
+			return ((t_list *)free_them_all(*all, coords, map, points));
+		}
+		ft_lstadd_back(&map, tmp);
+		all->max_row++;
+	}
+	return (map);
+}
