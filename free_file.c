972a0c9d3c4466ffc440bd65117f7b6082707b36
Nuria Ferron Ferriz <nuriaf207@gmail.com>
2023-09-06 21:34:54 +0200

    free functions

---
+++ free_file.c
@@ -0,0 +1,37 @@
+#include "fdf.h"
+
+void	free_matrix(t_structs all, char **matrix)
+{
+	int	i;
+
+	if (!matrix)
+		return ;
+	i = 0;
+	while (i < all.max_row)
+		free(matrix[i++]);
+	free(matrix);
+}
+
+void	free_list(t_list *lst)
+{
+	t_list	*tmp;
+
+	if (!lst)
+		return ;
+	while (lst)
+	{
+		free(lst->content);
+		tmp = lst->next;
+		free(lst);
+		lst  = tmp;
+	}
+}
+
+void	*free_them_all(t_structs all, char **mat, t_list *lst, t_point *p)
+{
+	free_matrix(all, mat);
+	free_list(lst);
+	free(p);
+	ft_printf("Uh-oh! There seems to be memory problems!\n");
+	return (NULL);
+}
