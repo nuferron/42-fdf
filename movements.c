cea5887301f5df34db1734ede9cde80e4ec4dc84
Nuria Ferron Ferriz <nuriaf207@gmail.com>
2023-09-11 17:27:57 +0200

    changed restart, so we're starting with isometric view

--- movements.c
+++ movements.c
@@ -3,13 +3,13 @@
 void	translation(t_structs *all, int key)
 {
 	if (key == RIGHT)
-		all->pox += 10;
+		all->design->pox += 30;
 	else if (key == LEFT)
-		all->pox -= 10;
+		all->design->pox -= 30;
 	else if (key == UP)
-		all->poy -= 10;
+		all->design->poy -= 30;
 	else if (key == DOWN)
-		all->poy += 10;
+		all->design->poy += 30;
 	update_map(all);
 }
 
@@ -19,9 +19,9 @@ void	rotation_x(t_structs *all, t_point *origin, t_point *final)
 	float	cosinus;
 	float	sinus;
 
-	cosinus = cos(PI / 9 * all->rotx);
-	sinus = sin(PI / 9 * all->rotx);
-	if (all->rotx != 0)
+	cosinus = cos(PI / 10 * all->design->rotx);
+	sinus = sin(PI / 10 * all->design->rotx);
+	if (all->design->rotx != 0)
 	{
 		y = origin->y;
 		origin->y = (y * cosinus - origin->z * sinus);
@@ -38,9 +38,9 @@ void	rotation_y(t_structs *all, t_point *origin, t_point *final)
 	float	cosinus;
 	float	sinus;
 
-	cosinus = cos(PI / 9 * all->roty);
-	sinus = sin(PI / 9 * all->roty);
-	if (all->roty != 0)
+	cosinus = cos(PI / 10 * all->design->roty);
+	sinus = sin(PI / 10 * all->design->roty);
+	if (all->design->roty != 0)
 	{
 		x = origin->x;
 		origin->x = (x * cosinus + origin->z * sinus);
@@ -57,9 +57,9 @@ void	rotation_z(t_structs *all, t_point *origin, t_point *final)
 	float	cosinus;
 	float	sinus;
 
-	cosinus = cos(PI / 9 * all->rotz);
-	sinus = sin(PI / 9 * all->rotz);
-	if (all->rotz != 0)
+	cosinus = cos(PI / 10 * all->design->rotz);
+	sinus = sin(PI / 10 * all->design->rotz);
+	if (all->design->rotz != 0)
 	{
 		x = origin->x;
 		origin->x = (x * cosinus - origin->y * sinus);
@@ -73,13 +73,53 @@ void	rotation_z(t_structs *all, t_point *origin, t_point *final)
 void	rotation(t_structs *all, int key)
 {
 	if (key == X)
-	{
-		all->rotx += 1;
-		printf("rotation x %d\n", all->rotx);
-	}
+		all->design->rotx += 1;
 	else if (key == Y)
-		all->roty += 1;
+		all->design->roty += 1;
 	else if (key == Z)
-		all->rotz += 1;
+		all->design->rotz += 1;
+	update_map(all);
+}
+
+void	height(t_structs *all, int key)
+{
+	if (key == PLUS)
+		all->design->height += 1;
+	else if (key == MINUS)
+		all->design->height -= 1;
+	update_map(all);
+}
+
+void	projection(t_structs *all, int key)
+{
+	if (key == ISO)
+	{
+		all->design->rotx = 1;
+		all->design->roty = 0;
+		all->design->rotz = 1;
+	}
+	else if (key == PAN)
+	{
+		all->design->rotx = 5;
+		all->design->roty = 0;
+		all->design->rotz = 0;
+	}
+	else if (key == BIRDY)
+	{
+		all->design->rotx = 0;
+		all->design->roty = 0;
+		all->design->rotz = 0;
+	}
+	update_map(all);
+}
+
+void	restart(t_structs *all)
+{
+	all->design->zoom = MAX_X / (all->max_col * 2);
+	all->design->rotx = 1;
+	all->design->roty = 0;
+	all->design->rotz = 1;
+	all->design->pox = (int)(MAX_X / 2);
+	all->design->poy = (int)(MAX_Y / 2);
 	update_map(all);
 }
